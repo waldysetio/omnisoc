ea0cf19eeb3fc94157b530338e3fa55c54da0912
Waldy Setiono <waldy.setiono@tiket.com>
2023-07-31 21:30:36 +0700

    refactor: Change the default mux to gorilla/mux

--- app/app.go
+++ app/app.go
@@ -1,13 +1,29 @@
 package app
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func Start() {
-	http.HandleFunc("/greethalo", greet)
-	http.HandleFunc("/accounts", getAllAccounts)
 
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	router := mux.NewRouter()
+
+	router.HandleFunc("/greethalo", greet).Methods(http.MethodGet)
+	router.HandleFunc("/accounts", addAccount).Methods(http.MethodPost)
+	router.HandleFunc("/accounts", getAllAccounts).Methods(http.MethodGet)
+	router.HandleFunc("/accounts/{account_id:[0-9]+}", getAccount).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
+
+func addAccount(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Request has been received")
+}
+
+func getAccount(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, vars["account_id"])
 }
